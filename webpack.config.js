5364fc5eb84a4eb9a6d5c97384a728ce2c4aa18e
fengqi <feengqi@gmail.com>
2022-04-30 18:46:08 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "build.js",
+    publicPath: "/abc"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "index.html"),
+      filename: "index.html"
+    })
+  ]
+};
